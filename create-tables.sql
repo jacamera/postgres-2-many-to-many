5aa19dab850cae1bcea3fd6729727c7e267ce76b
Jeff <jeff@jeffcamera.com>
2018-08-16 18:58:42 -0400

    added comments to CREATE TABLE statements

--- create-tables.sql
+++ create-tables.sql
@@ -1,3 +1,4 @@
+-- serial is a special data type in PostgreSQL. The column data type will just be an integer but in addition the database will automatically assign auto-incrementing numbers when new rows are inserted.
 CREATE TABLE link (
 	id serial PRIMARY KEY,
 	name text NOT NULL,
@@ -7,8 +8,9 @@ CREATE TABLE tag (
 	id serial PRIMARY KEY,
 	name text NOT NULL
 );
+-- The link_tag table's primary key is composed of both its foreign keys since duplicate identical relationships should not be allowed.
 CREATE TABLE link_tag (
-	link_id int NOT NULL REFERENCES link (id),
-	tag_id int NOT NULL REFERENCES tag (id),
+	link_id int NOT NULL FOREIGN KEY REFERENCES link (id),
+	tag_id int NOT NULL FOREIGN KEY REFERENCES tag (id),
 	PRIMARY KEY (link_id, tag_id)
 );
