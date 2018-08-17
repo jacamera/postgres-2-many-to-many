64ef7276a3aadd80a7bf6e3dace5851f55e6f186
Jeff <jeff@jeffcamera.com>
2018-08-16 21:28:08 -0400

    fixed foreign key syntax

--- create-tables.sql
+++ create-tables.sql
@@ -10,7 +10,7 @@ CREATE TABLE tag (
 );
 -- The link_tag table's primary key is composed of both its foreign keys since duplicate identical relationships should not be allowed.
 CREATE TABLE link_tag (
-	link_id int NOT NULL FOREIGN KEY REFERENCES link (id),
-	tag_id int NOT NULL FOREIGN KEY REFERENCES tag (id),
+	link_id int NOT NULL REFERENCES link (id),
+	tag_id int NOT NULL REFERENCES tag (id),
 	PRIMARY KEY (link_id, tag_id)
 );
